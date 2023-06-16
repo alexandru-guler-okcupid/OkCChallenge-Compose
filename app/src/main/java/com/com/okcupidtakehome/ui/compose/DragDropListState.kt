82c2c3619e9de3f167ca84592de6e396b25bbdf5
Alexandru Guler <alexandru@okcupid.com>
2023-06-16 14:02:20 -0500

    convert to grid initial (NOT WORKING)

--- app/src/main/java/com/com/okcupidtakehome/ui/compose/DragDropListState.kt
+++ app/src/main/java/com/com/okcupidtakehome/ui/compose/DragDropListState.kt
@@ -1,8 +1,8 @@
 package com.com.okcupidtakehome.ui.compose
 
-import androidx.compose.foundation.lazy.LazyListItemInfo
-import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.lazy.grid.LazyGridItemInfo
+import androidx.compose.foundation.lazy.grid.LazyGridState
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -11,44 +11,45 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.geometry.Offset
 import kotlinx.coroutines.Job
 
+private const val TAG = "DragDropListState"
+
 @Composable
 fun rememberDragDropListState(
-    lazyListState: LazyListState = rememberLazyListState(),
+    lazyGridState: LazyGridState = rememberLazyGridState(),
     onMove: (Int, Int) -> Unit,
 ): DragDropListState {
-    return remember { DragDropListState(lazyListState = lazyListState, onMove = onMove) }
+    return remember { DragDropListState(lazyGridState = lazyGridState, onMove = onMove) }
 }
 
 class DragDropListState(
-    val lazyListState: LazyListState,
+    val lazyGridState: LazyGridState,
     private val onMove: (Int, Int) -> Unit
 ) {
     var draggedDistance by mutableStateOf(0f)
 
     // used to obtain initial offsets on drag start
-    var initiallyDraggedElement by mutableStateOf<LazyListItemInfo?>(null)
+    var initiallyDraggedElement by mutableStateOf<LazyGridItemInfo?>(null)
 
     var currentIndexOfDraggedItem by mutableStateOf<Int?>(null)
 
     val initialOffsets: Pair<Int, Int>?
-        get() = initiallyDraggedElement?.let { Pair(it.offset, it.offsetEnd) }
-
+        get() = initiallyDraggedElement?.let { Pair(it.offset.y, it.offsetEnd) }
 
     val elementDisplacement: Float?
         get() = currentIndexOfDraggedItem
-            ?.let { lazyListState.getVisibleItemInfoFor(absoluteIndex = it) }
-            ?.let { item -> (initiallyDraggedElement?.offset ?: 0f).toFloat() + draggedDistance - item.offset }
+            ?.let { lazyGridState.getVisibleItemInfoFor(absoluteIndex = it) }
+            ?.let { item -> (initiallyDraggedElement?.offset?.y ?: 0f).toFloat() + draggedDistance - item.offset.y }
 
-    val currentElement: LazyListItemInfo?
+    val currentElement: LazyGridItemInfo?
         get() = currentIndexOfDraggedItem?.let {
-            lazyListState.getVisibleItemInfoFor(absoluteIndex = it)
+            lazyGridState.getVisibleItemInfoFor(absoluteIndex = it)
         }
 
     var overscrollJob by mutableStateOf<Job?>(null)
 
     fun onDragStart(offset: Offset) {
-        lazyListState.layoutInfo.visibleItemsInfo
-            .firstOrNull { item -> offset.y.toInt() in item.offset..(item.offset + item.size) }
+        lazyGridState.layoutInfo.visibleItemsInfo
+            .firstOrNull { item -> offset.y.toInt() in item.offset.y..(item.offset.y + item.size.height) }
             ?.also {
                 currentIndexOfDraggedItem = it.index
                 initiallyDraggedElement = it
@@ -65,36 +66,40 @@ class DragDropListState(
     fun onDrag(offset: Offset) {
         draggedDistance += offset.y
 
+
         initialOffsets?.let { (topOffset, bottomOffset) ->
             val startOffset = topOffset + draggedDistance
             val endOffset = bottomOffset + draggedDistance
 
             currentElement?.let { hovered ->
-                lazyListState.layoutInfo.visibleItemsInfo
-                    .filterNot { item -> item.offsetEnd < startOffset || item.offset > endOffset || hovered.index == item.index }
+                lazyGridState.layoutInfo.visibleItemsInfo
+                    .filterNot { item -> item.offsetEnd < startOffset || item.offset.y > endOffset || hovered.index == item.index }
                     .firstOrNull { item ->
-                        val delta = startOffset - hovered.offset
+                        val delta = startOffset - hovered.offset.y
                         when {
                             delta > 0 -> (endOffset > item.offsetEnd)
-                            else -> (startOffset < item.offset)
+                            else -> (startOffset < item.offset.y)
                         }
                     }
                     ?.also { item ->
-                        currentIndexOfDraggedItem?.let { current -> onMove.invoke(current, item.index) }
-                        currentIndexOfDraggedItem = item.index
+                        currentIndexOfDraggedItem?.let { current ->
+//                            onMove.invoke(current, item.index)
+                        }
+//                        currentIndexOfDraggedItem = item.index
                     }
             }
         }
     }
 
+
     fun checkForOverScroll(): Float {
         return initiallyDraggedElement?.let {
-            val startOffset = it.offset + draggedDistance
+            val startOffset = it.offset.y + draggedDistance
             val endOffset = it.offsetEnd + draggedDistance
 
             return@let when {
-                draggedDistance > 0 -> (endOffset - lazyListState.layoutInfo.viewportEndOffset).takeIf { diff -> diff > 0 }
-                draggedDistance < 0 -> (startOffset - lazyListState.layoutInfo.viewportStartOffset).takeIf { diff -> diff < 0 }
+                draggedDistance > 0 -> (endOffset - lazyGridState.layoutInfo.viewportEndOffset).takeIf { diff -> diff > 0 }
+                draggedDistance < 0 -> (startOffset - lazyGridState.layoutInfo.viewportStartOffset).takeIf { diff -> diff < 0 }
                 else -> null
             }
         } ?: 0f
@@ -102,19 +107,19 @@ class DragDropListState(
 }
 
 /*
-    LazyListItemInfo.index is the item's absolute index in the list
+    LazyGridState.index is the item's absolute index in the list
     Based on the item's "relative position" with the "currently top" visible item,
     this returns LazyListItemInfo corresponding to it
 */
-fun LazyListState.getVisibleItemInfoFor(absoluteIndex: Int): LazyListItemInfo? {
+fun LazyGridState.getVisibleItemInfoFor(absoluteIndex: Int): LazyGridItemInfo? {
     return this.layoutInfo.visibleItemsInfo.getOrNull(absoluteIndex - this.layoutInfo.visibleItemsInfo.first().index)
 }
 
 /*
   Bottom offset of the element in Vertical list
 */
-val LazyListItemInfo.offsetEnd: Int
-    get() = this.offset + this.size
+val LazyGridItemInfo.offsetEnd: Int
+    get() = this.offset.y + this.size.height
 
 /*
    Moving element in the list
--- app/src/main/java/com/com/okcupidtakehome/ui/compose/DraggableList.kt
+++ app/src/main/java/com/com/okcupidtakehome/ui/compose/DraggableList.kt
@@ -7,11 +7,15 @@ import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,8 +23,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
@@ -29,6 +34,8 @@ import androidx.compose.ui.unit.dp
 import java.util.UUID
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+
+private const val TAG = "DraggableList"
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -44,6 +51,72 @@ fun DraggableList(
 
     val dragDropListState = rememberDragDropListState(onMove = onMove)
 
+    LazyVerticalGrid(
+        state = dragDropListState.lazyGridState,
+        columns = GridCells.Fixed(3),
+        contentPadding = PaddingValues(vertical = 6.dp),
+        verticalArrangement = Arrangement.spacedBy(6.dp),
+        horizontalArrangement = Arrangement.spacedBy(6.dp),
+        modifier = modifier
+            .padding(horizontal = 6.dp)
+            .fillMaxWidth()
+            .fillMaxHeight(0.75f)
+            .pointerInput(Unit) {
+                detectDragGesturesAfterLongPress(
+                    onDrag = { change, offset ->
+                        change.consume()
+                        dragDropListState.onDrag(offset)
+
+                        if (overscrollJob?.isActive == true) {
+                            return@detectDragGesturesAfterLongPress
+                        }
+
+                        dragDropListState.checkForOverScroll()
+                            .takeIf { it != 0f }
+                            ?.let {
+                                overscrollJob = scope.launch { dragDropListState.lazyGridState.scrollBy(it) }
+                            }
+                            ?: run { overscrollJob?.cancel() }
+                    },
+                    onDragStart = { offset ->
+                        dragDropListState.onDragStart(offset)
+                    },
+                    onDragEnd = {
+                        dragDropListState.onDragInterrupted()
+                    },
+                    onDragCancel = {
+                        dragDropListState.onDragInterrupted()
+                    }
+                )
+            }
+    ) {
+        itemsIndexed(
+            items = list,
+            key = { index, item -> item.id  }
+        ) {index, item ->
+            Box(
+                modifier = Modifier
+                    .animateItemPlacement()
+                    .graphicsLayer {
+                        val offsetOrNull = dragDropListState.elementDisplacement.takeIf {
+                            index == dragDropListState.currentIndexOfDraggedItem
+                        }
+                        translationY = offsetOrNull ?: 0f
+                    }
+                    .fillMaxSize()
+                    .aspectRatio(1f)
+                    .background(Color.White)
+            ) {
+                Text(
+                    text = "Item ${item.title}",
+                    color = Color.Black,
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                )
+            }
+        }
+    }
+    /*
     LazyColumn(
         state = dragDropListState.lazyListState,
         verticalArrangement = Arrangement.spacedBy(16.dp),
@@ -85,13 +158,11 @@ fun DraggableList(
             Box(
                 modifier = Modifier
                     .animateItemPlacement()
-                    .composed {
+                    .graphicsLayer {
                         val offsetOrNull = dragDropListState.elementDisplacement.takeIf {
                             index == dragDropListState.currentIndexOfDraggedItem
                         }
-                        Modifier.graphicsLayer {
-                            translationY = offsetOrNull ?: 0f
-                        }
+                        translationY = offsetOrNull ?: 0f
                     }
                     .background(color = Color.White, shape = RoundedCornerShape(4.dp))
                     .fillMaxWidth()
@@ -105,6 +176,7 @@ fun DraggableList(
             }
         }
     }
+     */
 }
 
 data class ReorderItem(
