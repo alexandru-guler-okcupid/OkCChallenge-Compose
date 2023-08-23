792e161bdc6a8fc6cde4f2d0dd413ea91301d6bb
Alexandru Guler <alexandru@okcupid.com>
2023-08-23 15:49:39 -0500

    stuff

--- app/src/main/java/com/com/okcupidtakehome/MainActivityCompose.kt
+++ app/src/main/java/com/com/okcupidtakehome/MainActivityCompose.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Tab
 import androidx.compose.material.TabRow
